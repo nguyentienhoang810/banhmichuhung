46dd97fe69e70871fc70825bbd8d7cbd05904910
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-11-13 17:00:48 +0900

    make home frame and simple css

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,10 +2,53 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>TRANG CHU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+    <link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">    
+    <link rel="stylesheet" type="text/css" href="public/css/home.css">
+    <title>TRANG CHU</title>
 </head>
 <body>
-    TRANG CHU<br/>
-    WELCOME TO BANH MI CHU HUNG
+    <div class="container-fluid">
+
+        <div class="row section1">
+            <div class="col-md-4 head-contact-col">
+                <h6>HOT LINE: 0909999999</h6>
+            </div>
+            <div class="col-md-4 head-contact-col">
+                <i class="far fa-clock icon"></i>
+                <h6>T2-T7 08:00-21:00</h6>
+            </div>
+            <div class="col-md-4 head-contact-col">
+                <i class="fas fa-map-marker-alt icon"></i>
+                <h6>Bắc Giang quên lối về</h6>
+            </div>
+        
+            <div class="col-md-12 headline-content">
+                <h2>MAKING PEOPLE HAPPY</h2>
+            </div>
+            <div class="col-md-12 headline-content">
+                <h2>BANH MI CHU HUNG</h2>
+            </div>
+            <div class="col-md-12 headline-content">
+                <h2>NGON TU MIENG DAU TIEN</h2>
+            </div>
+            <div class="col-md-12 headline-content">
+                <a href="/order" class="button">Đặt hàng ngay</a>
+            </div>
+        </div>
+        {{-- END SECTION 1 --}}
+
+        <div class="row section2">
+            <h1>WELCOME TO BANH MI CHU HUNG</h1>
+        </div>
+
+        <div class="row section3">
+            <h1>WELCOME TO BANH MI CHU HUNG</h1>
+        </div>
+        
+    </div>
+
 </body>
 </html>
