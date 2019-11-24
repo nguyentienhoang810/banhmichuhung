2e13c8aa1e31547cc46ee097b4b187d5ca2d9ff5
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-11-24 16:59:33 +0900

    add feed back slide

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -112,6 +112,33 @@
             </div>{{-- end food container --}}
         </div>
 
+        <div class="row feedback-slide">
+            <div class="container feedback-title">
+                <span>Chúng mình được yêu thích như thế nào</span>
+            </div>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="public/images/background/background-yellow.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="public/images/background/background-white.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="public/images/background/background-yellow.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+
         <div class="row footer">
             <div class="container">
                 <img src="public/images/logo.jpg" alt="logo-banhmichuhung" class="footer-logo">
@@ -119,6 +146,7 @@
         </div>
         {{-- end footer --}}
     </div>
-
+    <script src="public/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
