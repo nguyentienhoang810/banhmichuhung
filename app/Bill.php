a6b107936493e8c20159a82e0a104f4146b55a54
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-12-15 15:39:20 +0900

    create model and build relationship

--- app/Bill.php
+++ app/Bill.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Bill extends Model
 {
     protected $table = "bills";
+
+    public function bill_detail() {
+        return $this->hasMany('App\BillDetail', 'id_bill', 'id');
+    }
+
+    public function customer() {
+        return $this->belongTo('App\Customer', 'id_customer', 'id');
+    }
 }
--- app/BillDetail.php
+++ app/BillDetail.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class BillDetail extends Model
 {
     protected $table = "bill_detail";
+
+    public function product() {
+        return $this->belongTo('App\Product', 'id_product', 'id');
+    }
+
+    public function bill() {
+        return $this->belongTo('App\Bill', 'id_bill', 'id');
+    }
 }
