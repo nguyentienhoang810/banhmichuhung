5fef06dc2d024cc563847c2080e3b17b7d0e55f9
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-12-17 18:01:44 +0900

    refator product card to template

--- resources/views/page/product_detail.blade.php
+++ resources/views/page/product_detail.blade.php
@@ -103,34 +103,7 @@
                     <div class="row">
                         @foreach ($sameProds as $prod)
                         <div class="col-sm-4">
-                            <div class="single-item">
-                                @if ($prod->promotion_price != 0)
-                                    <div class="ribbon-wrapper">
-                                        <div class="ribbon sale">
-                                            Sale
-                                        </div>
-                                    </div>
-                                @endif
-                                <div class="single-item-header">
-                                    <a href="product-detail/{{ $prod->id }}"><img src="public/source/image/product/{{ $prod->image }}" alt="" height="250px"></a>
-                                </div>
-                                <div class="single-item-body">
-                                    <p class="single-item-title">{{ $prod->name }}</p>
-                                    <p class="single-item-price">
-                                        @if ($prod->promotion_price == 0)
-                                            <span class="flash-sale">{{ $prod->unit_price }}</span>
-                                        @else
-                                            <span class="flash-del">{{ $prod->unit_price }}</span>
-                                            <span class="flash-sale">{{ $prod->promotion_price }}</span>
-                                        @endif
-                                    </p>
-                                </div>
-                                <div class="single-item-caption">
-                                    <a class="add-to-cart pull-left" href="#"><i class="fa fa-shopping-cart"></i></a>
-                                    <a class="beta-btn primary" href="product-detail/{{ $prod->id }}">Details <i class="fa fa-chevron-right"></i></a>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
+                            @include('prod')
                         </div>
                         @endforeach
                     </div>
--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -35,39 +35,10 @@
                         </div>
 
                         <div class="row">
-                            @foreach($newProducts as $newProduct)
+                            @foreach($newProducts as $prod)
                             <div class="col-sm-3">
                                 <div class="single-item">
-                                    @if ($newProduct->promotion_price != 0)
-                                    <div class="ribbon-wrapper">
-                                        <div class="ribbon sale">
-                                            Sale
-                                        </div>
-                                    </div>
-                                    @endif
-                                    <div class="single-item-header">
-                                        <a href="product-detail/{{ $newProduct->id }}"><img src="public/source/image/product/{{ $newProduct->image }}" alt="" height="250px"></a>
-                                    </div>
-                                    <div class="single-item-body">
-                                        <p class="single-item-title">{{ $newProduct->name }}</p>
-                                        <p class="single-item-price">
-                                            @if ($newProduct->promotion_price == 0)
-                                                <span class="flash-sale">{{ $newProduct->unit_price }}</span>
-                                            @else
-                                                <span class="flash-del">{{ $newProduct->unit_price }}</span>
-                                                <span class="flash-sale">{{ $newProduct->promotion_price }}</span>
-                                            @endif
-                                        </p>
-                                    </div>
-                                    <div class="single-item-caption">
-                                        <a class="add-to-cart pull-left" href="shopping_cart.html">
-                                            <i class="fa fa-shopping-cart"></i>
-                                        </a>
-                                        <a class="beta-btn primary" href="product-detail/{{ $newProduct->id }}">
-                                            Details<i class="fa fa-chevron-right"></i>
-                                        </a>
-                                        <div class="clearfix"></div>
-                                    </div>
+                                    @include('prod')
                                 </div>
                             </div>
                             @endforeach
@@ -87,23 +58,7 @@
                             @foreach ($promotionProducts as $prod)
                             <div class="col-sm-3">
                                 <div class="single-item">
-                                    <div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
-
-                                    <div class="single-item-header">
-                                        <a href="product-detail/{{ $newProduct->id }}"><img src="public/source/image/product/{{ $prod->image }}" alt="" height="250px"></a>
-                                    </div>
-                                    <div class="single-item-body">
-                                        <p class="single-item-title">{{ $prod->name }}</p>
-                                        <p class="single-item-price">
-                                            <span class="flash-del">{{ $prod->unit_price }}</span>
-                                            <span class="flash-sale">{{ $prod->promotion_price }}</span>
-                                        </p>
-                                    </div>
-                                    <div class="single-item-caption">
-                                        <a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-                                        <a class="beta-btn primary" href="product-detail/{{ $newProduct->id }}">Details <i class="fa fa-chevron-right"></i></a>
-                                        <div class="clearfix"></div>
-                                    </div>
+                                    @include('prod')
                                 </div>
                             </div>
                             @endforeach
