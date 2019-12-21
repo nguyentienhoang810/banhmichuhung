a914a87f2895e375c4b3efcbca165ab6e9108a13
nguyentienhoang <nguyentienhoang810@gmail.com>
2019-12-21 15:04:50 +0900

    create module, config and implement module service provider

---
+++ config/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'backend'=>[
+        'folder' => 'Backend',
+        'prefix_url' => 'admin',
+        'group_middleware' => []
+    ],
+    'frontend'=>[
+        'folder'=> 'Frontend',
+        'prefix_url' => '',
+        'group_middleware' => []
+    ],
+    'api'=>[
+        'folder' => 'Api',
+        'prefix_url' => 'api',
+        'group_middleware' => []
+    ]
+];
